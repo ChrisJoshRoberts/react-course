e24107108c51d776ccf72de0c5945417b0940735
ChrisJoshRoberts <chrisrobertscllective@gmail.com>
2024-10-17 16:14:49 +0200

    adding useState to NewPost

--- src/components/NewPosts.jsx
+++ src/components/NewPosts.jsx
@@ -1,12 +1,18 @@
 import classes from "./NewPost.module.css"
+import { useState } from "react"
 
 const NewPosts = () => {
+  const [output, setOutput] = useState('')
+  function changeBodyHandler(event) {
+    setOutput(event.target.value)
+  }
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{output}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
