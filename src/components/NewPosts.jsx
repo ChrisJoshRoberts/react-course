ee3fc6f8a7aea95a639e70e2386fbf4daf8b8adc
Chris Roberts <chrisrobertscollective@gmail.com>
2024-10-17 20:53:43 +0200

    adding sumbit handler

--- src/components/NewPosts.jsx
+++ src/components/NewPosts.jsx
@@ -1,20 +1,41 @@
 import classes from "./NewPost.module.css"
 import PropTypes from "prop-types"
+import { useState } from "react"
 
 
-const NewPosts = ({onBodyChange, onAuthorChange, onCancel}) => {
+const NewPosts = ({onCancel}) => {
+  const [output, setOutput] = useState('')
+  const [author, setAuthor] = useState('')
+  // Add a function to handle the change of the body input
+  function changeBodyHandler(event) {
+    setOutput(event.target.value)
+  }
+  // Add a function to handle the change of the author input
+  function changeAuthorHandler(event) {
+    setAuthor(event.target.value)
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: output,
+      author: author,
+    }
+    console.log(postData)
+    onCancel()
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required  onChange={onAuthorChange}/>
+        <input type="text" id="name" required  onChange={changeAuthorHandler}/>
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={onCancel}>Cancel</button>
+        <button id="cancelBtn" type="button" onClick={onCancel}>Cancel</button>
         <button>Submit</button>
       </div>
 
@@ -22,8 +43,6 @@ const NewPosts = ({onBodyChange, onAuthorChange, onCancel}) => {
   )
 }
 NewPosts.propTypes = {
-  onBodyChange: PropTypes.func,
-  onAuthorChange: PropTypes.func,
   onCancel: PropTypes.func
 }
 export default NewPosts
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,34 +1,21 @@
 import Post from './Post'
 import classes from './PostsList.module.css'
 import NewPosts from './NewPosts'
-import { useState } from 'react'
 import Modal from './Modal'
 import PropTypes from 'prop-types'
 
 const PostList = ({isPosting, onStopPosting}) => {
-  const [output, setOutput] = useState('')
-  const [author, setAuthor] = useState('')
-  // Add a function to handle the change of the body input
-  function changeBodyHandler(event) {
-    setOutput(event.target.value)
-  }
-  // Add a function to handle the change of the author input
-  function changeAuthorHandler(event) {
-    setAuthor(event.target.value)
-  }
   return (
   <>
     {isPosting && (
       <Modal onClose={onStopPosting}>
         <NewPosts
-          onBodyChange={changeBodyHandler}
-          onAuthorChange={changeAuthorHandler}
           onCancel={onStopPosting}
         />
       </Modal>
     )}
     <ul className={classes.posts}>
-      <Post author={author} body={output} />
+      
     </ul>
   </>
   )
