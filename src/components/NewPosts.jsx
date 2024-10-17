b8d3d3127e4b8a06730424650c75c96f5eddbc26
ChrisJoshRoberts <chrisrobertscllective@gmail.com>
2024-10-17 16:25:27 +0200

    lifting state up one level

--- src/components/NewPosts.jsx
+++ src/components/NewPosts.jsx
@@ -1,18 +1,13 @@
 import classes from "./NewPost.module.css"
-import { useState } from "react"
 
-const NewPosts = () => {
-  const [output, setOutput] = useState('')
-  function changeBodyHandler(event) {
-    setOutput(event.target.value)
-  }
+
+const NewPosts = (props) => {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{output}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,18 @@
 import Post from './Post'
 import classes from './PostsList.module.css'
 import NewPosts from './NewPosts'
+import { useState } from 'react'
 
 const PostList = () => {
+  const [output, setOutput] = useState('')
+  function changeBodyHandler(event) {
+    setOutput(event.target.value)
+  }
   return (
   <>
-    <NewPosts />
+    <NewPosts onBodyChange={changeBodyHandler} />
     <ul className={classes.posts}>
-      <Post author="John" body="React.js is awesome"/> 
+      <Post author="John" body={output} /> 
       <Post author="Jane" body="Check out the full course" />
       <Post author="Joe" body="I'm loving every minute of it" />
       <Post author="Jerry" body="I'm looking for Tom! have you seen him" />
