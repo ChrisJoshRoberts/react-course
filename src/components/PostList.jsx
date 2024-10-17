a19cf7b614bd626ba2155195efff06c6fce9777f
ChrisJoshRoberts <chrisrobertscllective@gmail.com>
2024-10-17 08:47:03 +0200

    creating PostList component and styling

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,10 +1,12 @@
 import Post from './Post'
-
+import classes from './PostsList.module.css'
 const PostList = () => {
   return (
-    <ul>
+    <ul className={classes.posts}>
       <Post author="John" body="React.js is awesome"/> 
       <Post author="Jane" body="Check out the full course" />
+      <Post author="Joe" body="I'm loving every minute of it" />
+      <Post author="Jerry" body="I'm looking for Tom! have you seen him" />
     </ul>
   )
 }
