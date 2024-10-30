898880189dca9d175a22161efbe69d3b659e4673
ChrisJoshRoberts <chrisrobertscllective@gmail.com>
2024-10-30 11:30:19 +0200

    adding dymanuic routes for pop PostDetails modal

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,20 @@
 import classes from './Post.module.css'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-const Post = ({author, body }) => {
+const Post = ({id, author, body }) => {
   return (
     <li className={classes.post}>
-      <h1 className={classes.author}>{author}</h1>
-      <p className={classes.text}>{body}</p>
+      <Link to={id}>
+        <h1 className={classes.author}>{author}</h1>
+        <p className={classes.text}>{body}</p>
+      </Link>
     </li>
   )
 }
 
 Post.propTypes = {
+  id: PropTypes.number,
   author: PropTypes.string,
   body: PropTypes.string
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import Posts, { loader as postsLoader } from './routes/Posts.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import NewPosts, {action as newPostAction } from './routes/NewPosts.jsx'
-import RootLayout from './routes/RootLayout.jsx'
+import RootLayout from './routes/RootLayout'
+import PostDetails, { loader as postDetailsLoader } from './routes/PostDetails'
 
 const router = createBrowserRouter([
   {path: '/', element: <RootLayout />, 
     children: [
-      {path: '/', element:  <Posts />, 
+      {
+        path: '/', element:  <Posts />, 
         loader: postsLoader,
         children: [
-          {path: '/create-post', element: <NewPosts />, action: newPostAction}
+          {path: '/create-post', element: <NewPosts />, action: newPostAction},
+          {path: '/:postId', element: <PostDetails />, loader: postDetailsLoader}
         ]
       }, 
     ]
